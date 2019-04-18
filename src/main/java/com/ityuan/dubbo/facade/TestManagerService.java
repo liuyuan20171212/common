28caedbbbc03d64dedf79e21047c1f70dc8c54f8
刘袁 <yuan.liu@caibeike.com>
2019-04-18 18:51:38 +0800

    first comm

---
+++ src/main/java/com/ityuan/dubbo/facade/TestManagerService.java
@@ -0,0 +1,15 @@
+/**
+ * Chengzai.com Inc.
+ * Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package com.ityuan.dubbo.facade;
+
+/**
+ *
+ * @author ityuan
+ * @version $Id: TestManagerService, v 0.1 2019-02-27 11:36 ityuan Exp $
+ */
+public interface TestManagerService {
+
+    String test01(String a);
+}
